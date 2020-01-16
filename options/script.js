9228f587319899af70634753661b69c92271d866
Benjamin Collet <benjamin.collet@protonmail.ch>
2020-01-17 00:01:48 +0100

    added icons and browser action

--- options/script.js
+++ options/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   browser.storage.sync.get(['resolverUrl', 'autoRedirect']).then((value) => {
-    document.querySelector('#resolver-url').value = value.resolverUrl || "https://doi.org/";
-    document.querySelector('#auto-redirect').checked = value.autoRedirect;
+    document.getElementById('resolver-url').value = value.resolverUrl || "https://doi.org/";
+    document.getElementById('auto-redirect').checked = value.autoRedirect;
   })
 });
 
 document.querySelector('form').addEventListener('change', function() {
   browser.storage.sync.set({
-    resolverUrl: document.querySelector('#resolver-url').value,
-    autoRedirect: document.querySelector('#auto-redirect').checked
+    resolverUrl: document.getElementById('resolver-url').value,
+    autoRedirect: document.getElementById('auto-redirect').checked
   });
 });
