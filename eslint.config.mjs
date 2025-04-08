9b0be7b827b51fe4ff0865d7799c5fc1fbeb94f7
Benjamin Collet <benjamin.collet@protonmail.ch>
2025-04-08 20:58:20 +0200

    Bump dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
